85e94c5572d897de660ffb2e31f05d1f1716aae9
Craig Robbins <crobbins@localhost.localdomain>
2014-01-03 16:22:40 +1000

    Added thanks to RangerMauve

--- buildwindows.bat
+++ buildwindows.bat
@@ -1,1 +1,5 @@
-gcc -o zenocpu.exe -I./ vpu/test.c vpu/backend/base.c vpu/backend/render.c vpu/backend/text.c vpu/fonts/bmfonts.c -lmingw32 -lSDLmain -lSDL
+:: Thanks to RangerMauve (from irc freenode) for compiling on 
+:: Windows, testing that it works on that platform and  giving
+:: me hints
+
+gcc -o zenocpu.exe -I. vpu/test.c vpu/backend/base.c vpu/backend/render.c vpu/backend/text.c vpu/fonts/bmfonts.c -lmingw32 -lSDLmain -lSDL
